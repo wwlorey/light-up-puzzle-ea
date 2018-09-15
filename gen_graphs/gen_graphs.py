b76f22f5deca61098849ec289982454889a81545
William Lorey <wwlorey@gmail.com>
2018-09-14 20:38:12 -0500

    Clean up graphing

--- gen_graphs/gen_graphs.py
+++ gen_graphs/gen_graphs.py
@@ -4,82 +4,98 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 
-log_files = ['../output/random_gen_log.txt', '../output/website_puzzle_log.txt']
+log_file_paths = ['../output/random_gen_log.txt', '../output/website_puzzle_log.txt']
+graph_titles = ['Randomly Generated Puzzles', 'Provided Puzzle']
 
-for log_file in log_files:
-    with open(log_file, 'r') as file:
-        file = file.read().split('\n')
-        file = [line for line in file[file.index('Run 1'):] if not line == '']
+for q in range(len(log_file_paths)):
+    with open(log_file_paths[q], 'r') as log_file:
+        # Create a list of lines from the log file, disregarding all config parmeters and empty lines
+        log_file = log_file.read().split('\n')
+        log_file = [line for line in log_file[log_file.index('Run 1'):] if not line == '']
+
         evals = []
-        fitnesses = []
-        best_fitnesses = []
-
         curr_evals = []
-        curr_fitnesses = []
-        curr_best_fitnesses = []
+        fits = []
+        curr_fits = []
+        best_fits = []
+        curr_best_fits = []
 
         curr_run = 1
 
-        for line in file:
+        # Segregate data from log file into appropriate lists
+        for line in log_file:
             if line[0] == 'R':
+                # This line has a run update
                 line = line.split()
                 run = int(line[1])
                 
                 if run > curr_run:
+                    # A new run has been encountered. Append the curr_* lists
                     curr_run = run
                     evals.append(curr_evals)
-                    fitnesses.append(curr_fitnesses)
-                    best_fitnesses.append(curr_best_fitnesses)
+                    fits.append(curr_fits)
+                    best_fits.append(curr_best_fits)
 
+                    # Reset the curr_* lists
                     curr_evals = []
-                    curr_fitnesses = []
-                    curr_best_fitnesses = []
+                    curr_fits = []
+                    curr_best_fits = []
 
             else:
+                # This line has eval and fitness data
                 line = line.split('\t')
 
+                # Append to the curr_* lists
                 curr_evals.append(int(line[0]))
-                curr_fitnesses.append(float(line[1]))
-                curr_best_fitnesses.append(float(line[2]))
+                curr_fits.append(float(line[1]))
+                curr_best_fits.append(float(line[2]))
         
+
         num_entries = len(evals)
-        lists = [evals, fitnesses, best_fitnesses]
+        experiment_data = [evals, fits, best_fits]
 
         # Average all values together
         # Condense the 2D lists into 1D
-        for l in range(len(lists)):
-            new = []
+        for k in range(len(experiment_data)):
+            avg = []
 
-            for i in range(len(lists[l][0])):
+            for i in range(len(experiment_data[k][0])):
                 summation = 0
 
-                for sub_l in lists[l]:
-                    summation += sub_l[i]
+                # Sum all data items in the slice of experiment data
+                for data_item in experiment_data[k]:
+                    summation += data_item[i]
                 
-                new.append(summation / num_entries)
+                # Average the data
+                avg.append(summation / num_entries)
         
-            lists[l] = new
+            # Replace the original data with the averaged version
+            experiment_data[k] = avg
 
-        eval_list = lists[0]
-        fitness_list = lists[1]
-        best_fitness_list = lists[2]
 
-        # Graph the result
-        plt.plot(eval_list, fitness_list, '-ro', linewidth = 2.0)
-        plt.plot(eval_list, best_fitness_list, '-bo', linewidth = 2.0)
+        # Reassign list names for clarity
+        evals = experiment_data[0]
+        fits = experiment_data[1]
+        best_fits = experiment_data[2]
+
+        # Plot the results
+        plt.plot(evals, fits, '-ro', linewidth = 2.0)
+        plt.plot(evals, best_fits, '-bo', linewidth = 2.0)
 
         plt.ylim(0, 1)
-        plt.xlim(0, eval_list[-1] + (len(eval_list) * 20))
+        plt.xlim(0, evals[-1] + (len(evals) * 20))
 
         red_patch = mpatches.Patch(color='red', label='Average Fitness')
         blue_patch = mpatches.Patch(color='blue', label='Average Best Fitness')
         plt.legend(handles=[blue_patch, red_patch])
+
+        plt.title('Evaluations versus Average Local Fitness and Evaluations versus Local\nBest Fitness for ' + graph_titles[q] + ', Averaged Over All Runs')
 
         # Include necessary labels
         plt.xlabel('Evaluations')
         plt.ylabel('Fitness\n(ratio of lit cells to all white cells)')
 
         # Save and close the plot
-        plt.savefig(log_file[:log_file.find('log')] + 'graph.png')
+        plt.savefig(log_file_paths[q][:log_file_paths[q].find('log')] + 'graph.png')
         plt.close()
             
